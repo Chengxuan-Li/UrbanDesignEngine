ca9520337ee90719e36da766ef787411e6835fdd
Chengxuan Li <lichengxuan@outlook.com>
2024-03-11 17:38:48 +0000

    Code cleanup Recast

--- System/Preview.cs
+++ System/Preview.cs
@@ -241,47 +241,69 @@ namespace System
             else return val;
         }
     }
-}
 
-namespace System.Runtime.CompilerServices
-{
-    internal static class RuntimeHelpers
+    public static class ArrayExtension
     {
-        /// <summary>
-        /// Slices the specified array using the specified range.
-        /// </summary>
-        public static T[] GetSubArray<T>(T[] array, Range range)
+        public static void Fill<T>(T[] array, T element)
         {
-            if (array == null)
+            for (int i = 0; i < array.Length; i++)
             {
-                throw new ArgumentNullException(nameof(array));
-            }
-
-            (int offset, int length) = range.GetOffsetAndLength(array.Length);
-
-            if (default(T) != null || typeof(T[]) == array.GetType())
-            {
-                // We know the type of the array to be exactly T[].
-
-                if (length == 0)
-                {
-                    return Array.Empty<T>();
-                }
-
-                var dest = new T[length];
-                Array.Copy(array, offset, dest, 0, length);
-                return dest;
-            }
-            else
-            {
-                // The array is actually a U[] where U:T.
-                var dest = (T[])Array.CreateInstance(array.GetType().GetElementType(), length);
-                Array.Copy(array, offset, dest, 0, length);
-                return dest;
+                array[i] = element;
             }
         }
+
+        public static void Fill<T>(T[] array, T element, int startIndex, int count)
+        {
+            int c = 0;
+            for (int i = startIndex; i < array.Length; i++)
+            {
+                array[i] = element;
+                c += 1;
+                if (c == count) return;
+            }
+            return;
+        }
+}
+
+    namespace System.Runtime.CompilerServices
+    {
+        internal static class RuntimeHelpers
+        {
+            /// <summary>
+            /// Slices the specified array using the specified range.
+            /// </summary>
+            public static T[] GetSubArray<T>(T[] array, Range range)
+            {
+                if (array == null)
+                {
+                    throw new ArgumentNullException(nameof(array));
+                }
+
+                (int offset, int length) = range.GetOffsetAndLength(array.Length);
+
+                if (default(T) != null || typeof(T[]) == array.GetType())
+                {
+                    // We know the type of the array to be exactly T[].
+
+                    if (length == 0)
+                    {
+                        return Array.Empty<T>();
+                    }
+
+                    var dest = new T[length];
+                    Array.Copy(array, offset, dest, 0, length);
+                    return dest;
+                }
+                else
+                {
+                    // The array is actually a U[] where U:T.
+                    var dest = (T[])Array.CreateInstance(array.GetType().GetElementType(), length);
+                    Array.Copy(array, offset, dest, 0, length);
+                    return dest;
+                }
+            }
+        }
+
     }
-
-
 
 }
